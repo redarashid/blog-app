88325bd38032827caa2761b9c4ba78b2fb6cd74d
redarashid <redarashid824@gmail.com>
2025-03-20 14:09:41 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import About from "./Pages/About";
+import Layout from "./components/_commons/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="" element={<Layout />} />
         <Route path="" element={<About />} />
       </Routes>
     </BrowserRouter>
