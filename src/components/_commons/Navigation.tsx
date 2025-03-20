2923499e4d8b2764ad3b5d1d56ccaccd1848ca10
redarashid <redarashid824@gmail.com>
2025-03-20 16:47:53 +0200

    first commit

--- src/components/_commons/Navigation.tsx
+++ src/components/_commons/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, DisclosureButton } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const Navigation = () => {
@@ -6,50 +6,64 @@ const Navigation = () => {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-              <div className="flex">
-                <div className=" flex space-x-4">
-                  <a
-                    href="#"
-                    className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium ">
-                    Posts
-                  </a>
-                  <a
-                    href="#"
-                    className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium  ">
-                    Categories
-                  </a>
-                  <a
-                    href="#"
-                    className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium  ">
-                    About
-                  </a>
-                </div>
-              </div>
-
-              <div className="-mr-2 flex md:hidden">
-                <Disclosure.Button className="p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700">
+          <div className="max-w-7xl mx-auto px-2">
+            <div className=" relative flex items-center justify-between h-16">
+              <div className=" absolute inset-y-0 left-0 flex items-center sm:hidden">
+                <DisclosureButton className=" relative inline-block items-center justify-between rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white ">
+                  <span className=" absolute -inset-0.5" />
+                  <span className=" sr-only">Open main menu</span>
                   {open ? (
-                    <XMarkIcon className="h-6 w-6" />
+                    <XMarkIcon className=" block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="h-6 w-6" />
+                    <Bars3Icon className=" block h-6 w-6" aria-hidden="false" />
                   )}
-                </Disclosure.Button>
+                </DisclosureButton>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className=" text-center">
+                  <div className=" hidden sm:ml-6 sm:block">
+                    <div className=" flex space-x-4">
+                      <a
+                        id="posts"
+                        href="#"
+                        className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium ">
+                        Posts
+                      </a>
+                      <a
+                        id="category"
+                        href="#"
+                        className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium  ">
+                        Categories
+                      </a>
+                      <a
+                        id="about"
+                        href="#"
+                        className=" text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium  ">
+                        About
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="-mr-2 flex md:hidden">
+                  <Disclosure.Button className="p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700">
+                    {open ? (
+                      <XMarkIcon className="h-6 w-6" />
+                    ) : (
+                      <Bars3Icon className="h-6 w-6" />
+                    )}
+                  </Disclosure.Button>
+                </div>
               </div>
             </div>
           </div>
 
           <Disclosure.Panel className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-white bg-gray-700">
+              <a className="block px-3 py-2 rounded-md text-white bg-gray-700">
                 Home
               </a>
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-white bg-gray-700">
+              <a className="block px-3 py-2 rounded-md text-white bg-gray-700">
                 About
               </a>
             </div>
