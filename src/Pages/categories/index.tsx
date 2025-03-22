b25158c3506469a91ab172d4c4710375e07868a5
redarashid <redarashid824@gmail.com>
2025-03-23 00:43:03 +0200

    first commit

--- src/Pages/categories/index.tsx
+++ src/Pages/categories/index.tsx
@@ -1,6 +1,28 @@
+import React from "react";
 import CategoryItem from "../../components/categories/CategoryItem";
+import { useGetCategoriesQuery } from "../../features/categories/category.api";
 
-const Category = () => {
+const Category: React.FC = () => {
+  const {
+    data: categories,
+    error,
+    isLoading,
+  } = useGetCategoriesQuery("categories");
+
+  if (isLoading) {
+    return (
+      <div className=" w-full h-56 text-center self-center">Loading...</div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className=" w-full h-56 text-center self-center">
+        ِAn error have occured
+      </div>
+    );
+  }
+
   return (
     <>
       <div className=" mx-auto max-w-2xl py-3 sm:py-10 lg:py-18 mb-4">
@@ -14,12 +36,13 @@ const Category = () => {
             fugiat aliqua.
           </p>
         </div>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-          
-        </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3"></div>
       </div>
       <div className=" grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
-        {categories && categories.map((category)=> <CategoryItem key={category.id} category={category} />)}
+        {categories &&
+          categories.map((category) => (
+            <CategoryItem key={category.id} category={category} />
+          ))}
       </div>
     </>
   );
