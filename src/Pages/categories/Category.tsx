7652f0c4213faa2b08a2975bbf392132b2401369
redarashid <redarashid824@gmail.com>
2025-03-22 23:13:53 +0200

    first commit

--- src/Pages/categories/Category.tsx
+++ src/Pages/categories/Category.tsx
@@ -1,10 +1,23 @@
-
 const Category = () => {
   return (
-    <div>
-      categories
+    <div className=" container mx-auto p-4 mb-4">
+      <div className=" mx-auto max-w-2xl py-3 sm:py-10 lg:py-18 mb-4 text-center">
+        <div className=" text-center">
+          <h1 className=" text-4xl font-bold sm:text-6xl tracking-tight">
+            Categories
+          </h1>
+          <p className=" mt-6 text-lg leading-8 text-gray-600">
+            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+            fugiat aliqua.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- src/features/categories/Category.api.ts
+++ src/features/categories/Category.api.ts
@@ -1,65 +1,55 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { Category } from '../../models/category.model'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Category } from "../../models/category.model";
 
 export const categoryApi = createApi({
+  reducerPath: "categoryApi",
 
-  // Define the slice name for the reducer in the store.
-  reducerPath: 'categoryApi',
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
 
-  // Configure the base query function for making API requests, specifying the base URL.
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api',}),
+  tagTypes: ["Category"],
 
-  // Define tagTypes to identify cache tags for entities. In this case, only 'Category' is specified.
-  tagTypes: ['Category'],
-
-  // Define the endpoints for the API, including queries and mutations.
   endpoints: (builder) => ({
-
-    // Define a query to fetch categories. It takes a 'name' parameter but doesn't use it in this case.
     getCategories: builder.query<Category[], string>({
-      query: (name) => '/categories',
-      providesTags: ['Category'],
+      query: (name) => "/categories",
+      providesTags: ["Category"],
     }),
 
-    // Define a mutation to add a new category. It takes a 'payload' parameter containing the category data.
     addNewCategory: builder.mutation({
       query: (payload) => ({
-        url: '/categories',
-        method: 'POST',
+        url: "/categories",
+        method: "POST",
         body: payload,
         headers: {
-          'Content-type': 'application/json; charset=UTF-8',
+          "Content-type": "application/json; charset=UTF-8",
         },
       }),
-      invalidatesTags: ['Category'],
+      invalidatesTags: ["Category"],
     }),
 
-    // Define a mutation to update an existing category. It takes a 'payload' parameter containing the updated category data.
     updateCategory: builder.mutation({
       query: (payload) => {
-        console.log(payload)
-        const { id, ...body } = payload
+        console.log(payload);
+        const { id, ...body } = payload;
         return {
           url: `/categories/${id}`,
-          method: 'PUT',
+          method: "PUT",
           body,
-        }
+        };
       },
-      invalidatesTags: ['Category'],
+      invalidatesTags: ["Category"],
     }),
 
     // Define a mutation to delete a category. It takes an 'id' parameter specifying the category to be deleted.
     deleteCategory: builder.mutation({
       query: (id) => ({
         url: `/categories/${id}`,
-        method: 'DELETE',
-        credentials: 'include',
+        method: "DELETE",
+        credentials: "include",
       }),
-      invalidatesTags: ['Category'],
+      invalidatesTags: ["Category"],
     }),
-
   }),
-})
+});
 
 // Destructure and export the query and mutation hooks for the defined endpoints.
 export const {
@@ -67,4 +57,4 @@ export const {
   useAddNewCategoryMutation,
   useUpdateCategoryMutation,
   useDeleteCategoryMutation,
-} = categoryApi
+} = categoryApi;
