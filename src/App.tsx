04ebe1360364cf3465b95d205284c4ae11fae5a5
redarashid <redarashid824@gmail.com>
2025-03-22 22:42:30 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import About from "./Pages/about/About";
 import Layout from "./components/_commons/Layout";
+import Category from "./components/categories/Category";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/about" element={<About />} />
+        <Route path="/category" element={<Category />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/categories/Category.tsx
+++ src/components/categories/Category.tsx
@@ -1,7 +1,7 @@
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <div id="category">Category</div>
   )
 }
 
