797ad30d605b5aebd639388a70e2dab863430c1b
redarashid <redarashid824@gmail.com>
2025-03-25 16:44:45 +0200

    first commit

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,1 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { categoryApi } from "./categories/category.api";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
+const store = configureStore({
+  reducer: {
+    [categoryApi.reducerPath]: categoryApi.reducer,
+  },
+  middleware: (getDefaulMiddleware) =>
+    getDefaulMiddleware().concat(categoryApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export default store;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import store from "./features/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
