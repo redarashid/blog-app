926d3fd8045f6149ccc5495304d512627ee4cf0b
redarashid <redarashid824@gmail.com>
2025-04-16 13:05:33 +0200

    blog-app

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import About from "./Pages/about/About";
 import Layout from "./components/_commons/Layout";
-import Category from "./Pages/categories";
 import NotFound from "./Pages/NotFound";
+import Categories from "./Pages/categories";
+import Home from "./Pages/posts";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={ <Home /> } />
+          <Route path="/categories" element={<Categories />} />
           <Route path="/about" element={<About />} />
-          <Route path="/categories" element={<Category />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
---
+++ src/Pages/posts/UpdatePost.tsx
@@ -0,0 +1,5 @@
+
+
+const UpdatePost: FC.React = ()=> {
+    
+}
