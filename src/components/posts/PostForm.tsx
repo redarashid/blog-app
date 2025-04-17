6bb51fc4db067d9d9d7c4cf932e0ce9c55d8d119
redarashid <redarashid824@gmail.com>
2025-04-17 15:29:51 +0200

    blog-app

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { useGetCategoriesQuery } from "../../features/categories/category.api";
+import { Category } from "../../models/category.model";
 import { Post } from "../../models/post.model";
 import { imageUrls } from "../../office/imageBank.lib";
 
@@ -8,10 +10,9 @@ interface PostFormProps {
 }
 
 const PostForm: React.FC<PostFormProps> = ({ post, onSubmit }) => {
+  const { data: categories } = useGetCategoriesQuery("categories");
+  const category = categories ? categories[0].name : "";
 
-    const [data: categories] = useGetCategoriesQuery('categories');
-    const category = categories ? categories[0].name : "";
-    
   const [formData, setFromData] = React.useState<Post>({
     title: post?.title || "",
     description: post?.description || "",
@@ -20,44 +21,52 @@ const PostForm: React.FC<PostFormProps> = ({ post, onSubmit }) => {
     category: post?.category || "",
   });
 
-
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >
+  ) => {
     const { name, value } = e.target;
-    setFromData((prevData)=> ({
-        ...prevData,
-        [name]: value,
-    }))
-  }
+    setFromData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     onSubmit(formData);
-  }
+  };
 
   // This is a non-parameterised function that will be executed when the user clicks on the ‘Regenerate’ button to select a new image, for example.
 
   const getRandomImageUrl = () => {
     const randomIndex = Math.floor(Math.random() * imageUrls.length);
-    setFromData((prevData)=> ({
+    setFromData((prevData) => ({
       ...prevData,
-      image: imageUrls[randomIndex]
-    }))
-  }
+      image: imageUrls[randomIndex],
+    }));
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label className=" block text-gray-700 font-bold mb-2">
-        Image
-      </label>
+      <label className=" block text-gray-700 font-bold mb-2">Image</label>
       <div className=" relative w-full bg-indigo-400/80 h-80 rounded-xl">
-        <img className=" w-full rounded-xl h-full object-cover" src={formData.image || imageUrls[0]} alt=""/>
+        <img
+          className=" w-full rounded-xl h-full object-cover"
+          src={formData.image || imageUrls[0]}
+          alt=""
+        />
       </div>
-      <div className=" absolute bg-blue-500 hover:bg-blue-700 text-white bottom-5 font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline cursor-pointer" onClick={getRandomImageUrl}>
+      <div
+        className=" absolute bg-blue-500 hover:bg-blue-700 text-white bottom-5 font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline cursor-pointer"
+        onClick={getRandomImageUrl}>
         Regenerate
       </div>
       <div className=" mb-4">
-        <label htmlFor="title" className=" block text-gray-700 font-bold mb-2">Title</label>
+        <label htmlFor="title" className=" block text-gray-700 font-bold mb-2">
+          Title
+        </label>
         <input
           type="text"
           id="title"
@@ -67,17 +76,40 @@ const PostForm: React.FC<PostFormProps> = ({ post, onSubmit }) => {
           className=" border w-full rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline              "
         />
       </div>
-      <div>
-        <label htmlFor="category" className="">Category</label>
-        <input
-          type=""
-          id="category"
-          name="category"
+      <div className=" mb-4">
+        <label htmlFor="category" className="">
+          Category
+        </label>
+        <select id="category" name="category" onChange={handleChange}>
+          {categories &&
+            categories.map((category: Category) => (
+              <option
+                key={category.id}
+                value={formData.category}
+                selected={category.name === formData.category}>
+                {category.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div className=" mb-4">
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          value={formData.description}
           onChange={handleChange}
         />
       </div>
+      <div className=" flex flex-row justify-end mt-2">
+        <button
+          type="submit"
+          className=" absolute bg-blue-500 hover:bg-blue-700 text-white bottom-5 font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline cursor-pointer">
+          Submit
+        </button>
+      </div>
     </form>
-  )
+  );
 };
 
 export default PostForm;
